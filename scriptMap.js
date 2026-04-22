61bbd3d07a73affe1925f8e05841c91d44e732f9
greya734 <rosajoshu@gmail.com>
2026-04-23 08:57:44 +1100

    json
    rapartir sur du JSON plutôt que de la réception de flux

--- scriptMap.js
+++ scriptMap.js
@@ -10,20 +10,10 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', { maxZ
 var marker = L.marker([-22.2758, 166.4572]).addTo(map);
 marker.bindPopup("<b>Nouméa</b>").openPopup();
 
-
-$.ajax({
-    url : '/api/explore/v2.1/catalog/datasets/bornes-de-recharge-pour-vehicules-electriques/records?limit=20',
-})
-
-
-
-/*$.getJSON('placeholder.json', function(data){
+$.getJSON('placeholder.json', function(data){
     $.each(data, function(index, personne){
         console.log(personne.name)
 
-        var marker = L.marker([personne.address.geo.lat, personne.address.geo.lng]).addTo(map)
-        marker.bindPopup(
-            "<p>" + personne.name + "</p><br>" +
-            "<a href='mailto:" + personne.email + "'>" + personne.email + "</a>").openPopup();
+        var marker = L.marker([borne.geo_shape.geometry.geo_point_2d.lat, borne.geo_shape.geometry.geo_point_2d.lon]).addTo(map)
     })
-})*/ 
+})
