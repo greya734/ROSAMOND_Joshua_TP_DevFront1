2922daeacd4ee10cb560a0a4827ace98b15106e4
greya734 <rosajoshu@gmail.com>
2026-05-18 16:34:45 +1100

    Barre de recherche fonctionnelle

--- scriptMap.js
+++ scriptMap.js
@@ -1,5 +1,3 @@
-
-
 var map = L.map('map',{
     center : [-22.2758, 166.4572],
     zoom: 13
@@ -10,12 +8,20 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', { maxZ
 var marker = L.marker([-22.2758, 166.4572]).addTo(map);
 marker.bindPopup("<b>Nouméa</b>").openPopup();
 
+// Stocker toutes les stations et leurs marqueurs
+var allStations = [];
+var allMarkers = [];
+
 $.getJSON('placeholder.json', function(data){
     console.log('Total :', data.total_count)
 
     $.each(data.results, function(index, station){
         var marker = L.marker([station.geo_point_2d.lat, station.geo_point_2d.lon]).addTo(map);
-        
+
+        // Stocker la station et son marqueur
+        allStations.push(station);
+        allMarkers.push(marker);
+
         marker.on('click', function(){
             console.log('Marqueur cliqué :', station.nom_station)
             document.getElementById('infos').innerHTML=
@@ -30,14 +36,48 @@ $.getJSON('placeholder.json', function(data){
                         <li>Observations : ${station.observations_stations}</li>
                     </ul>
                 `
-            //ouvvrir barre latérale
             $('#barre_laterale').removeClass('fermee')
         })
-
     })
-    //fermer barre latérale
-    $('#btn-fermer').on('click', function(){
-    $('#barre_laterale').addClass('fermee')
-})
 
+    // Fermer la barre latérale
+    $('#btn-fermer').on('click', function(){
+        $('#barre_laterale').addClass('fermee')
+    })
+
+    // Gestion de la recherche
+    $('form').on('submit', function(e){
+        e.preventDefault();
+
+        var critere = $('.option-critere').val();
+        var valeur = $('.critere').val().trim().toLowerCase();
+
+        allMarkers.forEach(function(marker, index){
+            var station = allStations[index];
+            var champStation = String(station[critere] ?? '').toLowerCase();
+
+            if (valeur === '' || champStation.includes(valeur)) {
+                // Afficher le marqueur s'il n'est pas déjà sur la carte
+                if (!map.hasLayer(marker)) {
+                    marker.addTo(map);
+                }
+            } else {
+                // Masquer le marqueur
+                if (map.hasLayer(marker)) {
+                    map.removeLayer(marker);
+                }
+            }
+        })
+    })
+
+    // Réinitialiser l'affichage si le champ de recherche est vidé
+    $('.critere').on('input', function(){
+        if ($(this).val().trim() === '') {
+            allMarkers.forEach(function(marker){
+                if (!map.hasLayer(marker)) {
+                    marker.addTo(map);
+                }
+            })
+        }
+    })
 })
